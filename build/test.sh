22b4aee73e7bf35adf1b18ebd16feedd66c70ccf
Florian Burger <florianburger03@gmail.com>
2024-06-16 09:54:34 +0200

    changed interface for test script

--- build/test.sh
+++ build/test.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-# CONFIG
-if [ -z "$1" ]
-then
-    CONFIG=debug
-else
-    CONFIG=$1
-fi
+# optional arguments
+CONFIG=debug
+TEST=test
+while getopts "c:t:" opt; do
+    case "${opt}" in
+        c)
+            CONFIG=${OPTARG};;
+        t)
+            TEST=${OPTARG};;
+        *)
+            exit;;
+    esac
+done
 
 # premake -> make -> run test
-premake5 gmake --file="build/premake5.lua" && make config=$CONFIG --quiet -C build && bin/${CONFIG}_test/test
+premake5 gmake --file="build/premake5.lua" && make config=$CONFIG -C build && bin/${CONFIG}_${TEST}/${TEST}

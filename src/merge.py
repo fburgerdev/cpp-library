72f167a01d5ebc6e9429e47b75340166b018cc1d
Florian Burger <florianburger03@gmail.com>
2024-06-23 21:40:49 +0200

    fixed small hppmerge problem

--- src/merge.py
+++ src/merge.py
@@ -5,4 +5,5 @@ workspace_config = json.load(open('cpp-library.json'))
 header_name = workspace_config["project"].lower()
 
 # merge
+pathlib.Path(f"include/{header_name}").mkdir(parents=True, exist_ok=True)
 os.system(f'python3 cpp-library/modules/hppmerge/hppmerge.py src/{header_name}.hpp include/{header_name}/{header_name}.hpp src')
